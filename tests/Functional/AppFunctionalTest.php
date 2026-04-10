d3ca96366e18f15aadf3d28bc351fb907f46d2b8
fournest sebastien <sebfour85@gmail.com>
2026-04-10 16:30:34 +0200

    Refactor templates for improved styling and user experience
    - Updated login form layout and added user feedback messages.
    - Enhanced tarif forms and index pages for better readability and usability.
    - Improved user profile edit and show pages with consistent styling.
    - Refined visit forms and detail views for clarity and user interaction.
    - Adjusted functional tests for better validation and clarity in assertions.

--- tests/Functional/AppFunctionalTest.php
+++ tests/Functional/AppFunctionalTest.php
@@ -13,20 +13,20 @@ class AppFunctionalTest extends WebTestCase
     {
         $reservation = new Reservation();
         $statutAttendue = 'EN COURS';
-        $statutIncorrecte = 'TERMINE'; 
         $reservation->setStatut($statutAttendue);
 
-        
-        $this->assertSame($statutIncorrecte, $reservation->getStatut(),
-            "*** CE TEST DOIT ÉCHOUER POUR LA DÉMO ! *** La valeur 'statut' devrait être 'TERMINE' mais est 'EN COURS'."
+        // Vérifie que le statut retourné correspond à celui défini
+        $this->assertSame($statutAttendue, $reservation->getStatut(),
+            "La valeur 'statut' doit être 'EN COURS'."
         );
     }
 
     
     public function testHomePageSuccess(): void
     {
-        
         $client = static::createClient();
+        // suivre les redirections pour atteindre la page finale
+        $client->followRedirects(true);
         $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful(
